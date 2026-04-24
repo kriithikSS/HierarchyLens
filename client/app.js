d493f9ff7b17b1f0083c987a7b5e92836cff214b
kriithikSS <kriithik05@gmail.com>
2026-04-24 18:47:38 +0530

    config: set production API URL to Render deployment

--- client/app.js
+++ client/app.js
@@ -1,6 +1,6 @@
 const API_BASE = window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1'
   ? 'http://localhost:3001'
-  : '';
+  : 'https://hierarchylens-api.onrender.com';
 
 function loadExample() {
   document.getElementById('nodeInput').value =
